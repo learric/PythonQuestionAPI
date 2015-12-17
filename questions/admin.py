707f2e7fab1dbe141b0a89270b16f3685374ee16
Rick Lear <learric@gmail.com>
2015-12-16 20:18:53 -0600

    update admin

--- questions/admin.py
+++ questions/admin.py
@@ -1,15 +1,34 @@
 from django.contrib import admin
 
-from .models import Question, Answer
+from .models import Question
 
 
-class AnswerInline(admin.StackedInline):
-    model = Answer
-    extra = 1
-    max_num = 1
+class QuestionForm(admin.ModelAdmin):
+    fieldsets = (
+        ('Question Text', {
+            'fields': ('first_line',)
+        }),
+        ('Additional Text', {
+            'fields': ('last_line',),
+            'classes': ('collapse',)
+        }),
+        ('Incorrect Answers', {
+            'fields': ('first_choice', 'second_choice', 'third_choice')
+        }),
+        ('Correct Answer', {
+            'fields': ('correct_choice',)
+        }),
+        ('Question Category', {
+            'fields': ('category',)
+        })
+    )
+    list_display = ('first_line', 'category')
+    list_filter = ['category']
+    search_fields = ['first_line']
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    inlines = [AnswerInline]
+    list_display = ('first_line', 'category')
 
-admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Question, QuestionForm)
--- questions/models.py
+++ questions/models.py
@@ -8,20 +8,16 @@ class Question(models.Model):
         ('Geography', 'Geography'),
         ('General', 'General')
     )
-    first_line = models.CharField(max_length=200)
-    last_line = models.CharField(max_length=200, blank=True)
+
+    first_line = models.TextField("Main text", max_length=200)
+    last_line = models.TextField("Extra text", max_length=200, blank=True, help_text="Use this area for second line")
+
+    first_choice = models.CharField(max_length=200)
+    second_choice = models.CharField(max_length=200)
+    third_choice = models.CharField(max_length=200)
+    correct_choice = models.CharField(max_length=200, help_text="This MUST be the correct answer")
+
     category = models.CharField(max_length=200, choices=CATEGORY_CHOICES)
 
     def __str__(self):
         return self.first_line
-
-
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    first_choice = models.CharField(max_length=200)
-    second_choice = models.CharField(max_length=200)
-    third_choice = models.CharField(max_length=200)
-    correct_choice = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.first_choice
